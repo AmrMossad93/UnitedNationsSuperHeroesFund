a3e161437a8d594ea48ee03271352a394950d203
AmrMossad93 <amr@unilearn.com.eg>
2021-04-30 06:19:38 +0200

    AMS: Home Structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home-page',
+    loadChildren: () => import('./Pages/Home-Page/home.module').then(c => c.HomeModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home-page',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
